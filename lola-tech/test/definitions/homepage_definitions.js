d0d76bd18e2325415d06500b485ee7fd3231d8ee
danielhuluban <huluban_daniel@yahoo.com>
2020-05-12 12:13:39 +0300

    fix settings

--- lola-tech/test/definitions/homepage_definitions.js
+++ lola-tech/test/definitions/homepage_definitions.js
@@ -1,9 +1,6 @@
-import {Given } from 'cucumber';
-import HomePage from '../pageobjects/homepage.js';
+import { Given } from "cucumber";
+import HomePage from "../pageobjects/homepage.js";
 
-
-Given(/^I access the base URL$/, async function() {
-    HomePage.openURL();
-  });
-
- 
+Given(/^I access the base URL$/, async function () {
+  HomePage.openURL();
+});
