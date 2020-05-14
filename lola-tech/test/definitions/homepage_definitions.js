7961d874f8c59cf1aca60e8ad14c73febdd90802
irimiecernea <irimie.cernea@lola.tech>
2020-05-14 14:46:59 +0300

    removed async from definition

--- lola-tech/test/definitions/homepage_definitions.js
+++ lola-tech/test/definitions/homepage_definitions.js
@@ -1,6 +1,6 @@
 import { Given } from "cucumber";
 import HomePage from "../pageobjects/homepage.js";
 
-Given(/^I access the base URL$/, async function () {
+Given(/^I access the base URL$/, function () {
   HomePage.openURL();
 });
